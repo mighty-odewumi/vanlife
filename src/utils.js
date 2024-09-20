618ba50943d5a0d91587099b4cb8563f485f1bae
gr1ntch <63327841+mighty-odewumi@users.noreply.github.com>
2024-09-20 16:22:51 +0000

    Refactor: created function to check for App auth update and protect routes

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,25 @@
 import { redirect } from "react-router-dom";
+import { auth } from "./api";
+import { onAuthStateChanged } from "firebase/auth";
 
 export async function requireAuth(request) {
-
   const url = new URL(request.url).pathname;
-  // console.log(url);
 
-  const isLoggedIn = localStorage.getItem("loggedIn");
+  // Create a Promise to check the user's authentication state
+  const userPromise = new Promise((resolve, reject) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        resolve(user);  // User is signed in
+      } else {
+        reject();  // User is not signed in
+      }
+    });
+  });
 
-  if (!isLoggedIn) {
-    const response = redirect(`/login?message=You have to log in&redirectTo=${url}`);
-    // response.body = true;
-    return response;
+  try {
+    await userPromise; // Wait for the user to be determined
+    return null; // User is authenticated, continue as normal
+  } catch {
+    return redirect(`/login?message=You have to log in&redirectTo=${url}`); // Redirect if not authenticated
   }
-  
-  return null;
 }
